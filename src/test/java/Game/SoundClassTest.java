93908ea229fcdbd75869ff1aa93fb66ba00764f2
Mime-135 <eseaig99@gmail.com>
2020-12-11 09:31:51 -0800

    Junit Testing begun

--- src/test/java/Game/SoundClassTest.java
+++ src/test/java/Game/SoundClassTest.java
@@ -1,10 +1,21 @@
 package Game;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SoundClassTest {
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("I execute before each class");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("I execute after each class");
+    }
 
     @Test
     public void playSong() {
--- src/test/java/Game/StartingGameScreenTest.java
+++ src/test/java/Game/StartingGameScreenTest.java
@@ -10,37 +10,54 @@ public class StartingGameScreenTest {
 
     @Before
     public void setUp() throws Exception {
+        System.out.println("I execute before each class");
     }
 
     @After
     public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void startingGameScreen() {
+        System.out.println("I execute after each class");
     }
 
     @Test
     public void run() {
+        //To test that the start game screen is set up properly
+        StartingGameScreen start = new StartingGameScreen();
+        start.run();
+        System.out.println("Started game screen");
     }
 
     @Test
     public void get_Theme() {
+        //Java Swing GUI
+        //Function tested during black box testing when running the game, interface testing and integration testing.
+        //It depends on clicked radio buttons.
     }
 
     @Test
     public void get_Difficulty() {
+        //Java Swing GUI
+        //Function tested during black box testing when running the game, interface testing and integration testing.
+        //It depends on clicked radio buttons.
     }
 
     @Test
     public void get_Item_type() {
+        //Java Swing GUI
+        //Function tested during black box testing when running the game, interface testing and integration testing.
+        //It depends on clicked radio buttons.
     }
 
     @Test
     public void get_Item_num() {
+        //Java Swing GUI
+        //Function tested during black box testing when running the game, interface testing and integration testing.
+        //It depends on clicked radio buttons.
     }
 
     @Test
     public void get_Timer() {
+        //Java Swing GUI
+        //Function tested during black box testing when running the game, interface testing and integration testing.
+        //It depends on clicked radio buttons.
     }
 }
